a18217144646f535d7b2f82555d3d38d1f4007dc
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-01-31 21:43:45 +0100

    rename internal vars to improve expressiveness

--- src/berlin_clock/core.clj
+++ src/berlin_clock/core.clj
@@ -2,19 +2,19 @@
   (:require [clojure.string :as str]))
 
 (defn to-berlin-single-minutes-row [time]
-  (let [minutes (-> (second (str/split time #":"))
-                    (Integer/parseInt)
-                    (mod 5))]
-    (apply str (concat (take minutes (cycle [\Y])) (repeat (- 4 minutes) \O)))))
+  (let [single-minutes (-> (second (str/split time #":"))
+                           (Integer/parseInt)
+                           (mod 5))]
+    (apply str (concat (take single-minutes (cycle [\Y])) (repeat (- 4 single-minutes) \O)))))
 
 (defn to-berlin-five-minutes-row [time]
-  (let [minutes (-> (second (str/split time #":"))
-                    (Integer/parseInt)
-                    (quot 5))]
-    (apply str (concat (take minutes (cycle [\Y \Y \R])) (repeat (- 11 minutes) \O)))))
+  (let [five-minutes (-> (second (str/split time #":"))
+                         (Integer/parseInt)
+                         (quot 5))]
+    (apply str (concat (take five-minutes (cycle [\Y \Y \R])) (repeat (- 11 five-minutes) \O)))))
 
 (defn to-berlin-single-hours-row [time]
-  (let [hours (-> (first (str/split time #":"))
-                  (Integer/parseInt)
-                  (quot 5))]
-    (apply str (concat (take hours (cycle [\R])) (repeat (- 4 hours) \O)))))
+  (let [single-hours (-> (first (str/split time #":"))
+                         (Integer/parseInt)
+                         (mod 5))]
+    (apply str (concat (take single-hours (cycle [\R])) (repeat (- 4 single-hours) \O)))))
