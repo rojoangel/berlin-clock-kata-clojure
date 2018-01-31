3037d43ac12e0b8cd0fbd55f10313e5d85d3273e
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-01-31 22:47:44 +0100

    extract all parameters for to-berlin-single-minutes-row before extracting helper method

--- src/berlin_clock/core.clj
+++ src/berlin_clock/core.clj
@@ -1,35 +1,26 @@
 (ns berlin-clock.core
   (:require [berlin-clock.time :as time]))
 
-(defn to-single-minutes [time]
-  (-> time
-      (time/minutes)
-      (mod 5)))
-
 (defn to-berlin-single-minutes-row [time]
-  (let [single-minutes (to-single-minutes time)]
-    (apply str (concat (take single-minutes (cycle [\Y])) (repeat (- 4 single-minutes) \O)))))
+  (let [time-conversion-f time/to-single-minutes
+        on-pattern [\Y]
+        fill-lamp \O
+        row-length 4
+        single-minutes (time-conversion-f time)]
+    (apply str (concat (take single-minutes (cycle on-pattern)) (repeat (- row-length single-minutes) fill-lamp)))))
 
 (defn to-berlin-five-minutes-row [time]
-  (let [five-minutes (-> time
-                         (time/minutes)
-                         (quot 5))]
+  (let [five-minutes (time/to-five-minutes time)]
     (apply str (concat (take five-minutes (cycle [\Y \Y \R])) (repeat (- 11 five-minutes) \O)))))
 
 (defn to-berlin-single-hours-row [time]
-  (let [single-hours (-> time
-                         (time/hours)
-                         (mod 5))]
+  (let [single-hours (time/to-single-hours time)]
     (apply str (concat (take single-hours (cycle [\R])) (repeat (- 4 single-hours) \O)))))
 
 (defn to-berlin-five-hours-row [time]
-  (let [five-hours (-> time
-                       (time/hours)
-                       (quot 5))]
+  (let [five-hours (time/to-five-hours time)]
     (apply str (concat (take five-hours (cycle [\R])) (repeat (- 4 five-hours) \O)))))
 
 (defn to-berlin-seconds-lamp [time]
-  (let [seconds (-> time
-                    (time/seconds)
-                    (mod 2))]
+  (let [seconds (time/to-seconds time)]
     (apply str (concat (take seconds (cycle [\O])) (repeat (- 1 seconds) \Y)))))
--- src/berlin_clock/time.clj
+++ src/berlin_clock/time.clj
@@ -7,17 +7,42 @@
 (defn- to-elements [time]
   (map parse-int (str/split time #":")))
 
-(defn minutes [time]
+(defn- minutes [time]
   (->> time
        (to-elements)
        (second)))
 
-(defn hours [time]
+(defn- hours [time]
   (->> time
        (to-elements)
        (first)))
 
-(defn seconds [time]
+(defn- seconds [time]
   (->> time
        (to-elements)
        (last)))
+
+(defn to-single-minutes [time]
+  (-> time
+      (minutes)
+      (mod 5)))
+
+(defn to-five-minutes [time]
+  (-> time
+      (minutes)
+      (quot 5)))
+
+(defn to-single-hours [time]
+  (-> time
+      (hours)
+      (mod 5)))
+
+(defn to-five-hours [time]
+  (-> time
+      (hours)
+      (quot 5)))
+
+(defn to-seconds [time]
+  (-> time
+      (seconds)
+      (mod 2)))
