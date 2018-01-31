aa1dccdd9246ccde4280a9be8bdb1bc9aa1ebcde
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-01-31 22:11:33 +0100

    refactor extract time->components helper function

--- src/berlin_clock/core.clj
+++ src/berlin_clock/core.clj
@@ -1,32 +1,45 @@
 (ns berlin-clock.core
   (:require [clojure.string :as str]))
 
+(defn- time->components [time]
+  (str/split time #":"))
+
 (defn to-berlin-single-minutes-row [time]
-  (let [single-minutes (-> (second (str/split time #":"))
+  (let [single-minutes (-> time
+                           (time->components)
+                           (second)
                            (Integer/parseInt)
                            (mod 5))]
     (apply str (concat (take single-minutes (cycle [\Y])) (repeat (- 4 single-minutes) \O)))))
 
 (defn to-berlin-five-minutes-row [time]
-  (let [five-minutes (-> (second (str/split time #":"))
+  (let [five-minutes (-> time
+                         (time->components)
+                         (second)
                          (Integer/parseInt)
                          (quot 5))]
     (apply str (concat (take five-minutes (cycle [\Y \Y \R])) (repeat (- 11 five-minutes) \O)))))
 
 (defn to-berlin-single-hours-row [time]
-  (let [single-hours (-> (first (str/split time #":"))
+  (let [single-hours (-> time
+                         (time->components)
+                         (first)
                          (Integer/parseInt)
                          (mod 5))]
     (apply str (concat (take single-hours (cycle [\R])) (repeat (- 4 single-hours) \O)))))
 
 (defn to-berlin-five-hours-row [time]
-  (let [five-hours (-> (first (str/split time #":"))
+  (let [five-hours (-> time
+                       (time->components)
+                       (first)
                        (Integer/parseInt)
                        (quot 5))]
     (apply str (concat (take five-hours (cycle [\R])) (repeat (- 4 five-hours) \O)))))
 
 (defn to-berlin-seconds-lamp [time]
-  (let [seconds (-> (last (str/split time #":"))
+  (let [seconds (-> time
+                    (time->components)
+                    (last)
                     (Integer/parseInt)
                     (mod 2))]
     (apply str (concat (take seconds (cycle [\O])) (repeat (- 1 seconds) \Y)))))
