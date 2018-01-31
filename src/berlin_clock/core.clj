9a2d3a28d8bb3d1d2fb250d91c9d871ae2365967
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-01-31 22:28:07 +0100

    extract hours and seconds helper functions and get rid on intermediate helper function

--- src/berlin_clock/core.clj
+++ src/berlin_clock/core.clj
@@ -7,13 +7,20 @@
 (defn- time->components [time]
   (map parse-int (str/split time #":")))
 
-(defn- get-time-element [time f]
-  (-> time
-      (time->components)
-      (f)))
-
 (defn- minutes [time]
-  (get-time-element time second))
+  (->> time
+       (time->components)
+       (second)))
+
+(defn- hours [time]
+  (->> time
+       (time->components)
+       (first)))
+
+(defn- seconds [time]
+  (->> time
+       (time->components)
+       (last)))
 
 (defn to-berlin-single-minutes-row [time]
   (let [single-minutes (-> time
@@ -29,21 +36,18 @@
 
 (defn to-berlin-single-hours-row [time]
   (let [single-hours (-> time
-                         (time->components)
-                         (first)
+                         (hours)
                          (mod 5))]
     (apply str (concat (take single-hours (cycle [\R])) (repeat (- 4 single-hours) \O)))))
 
 (defn to-berlin-five-hours-row [time]
   (let [five-hours (-> time
-                       (time->components)
-                       (first)
+                       (hours)
                        (quot 5))]
     (apply str (concat (take five-hours (cycle [\R])) (repeat (- 4 five-hours) \O)))))
 
 (defn to-berlin-seconds-lamp [time]
   (let [seconds (-> time
-                    (time->components)
-                    (last)
+                    (seconds)
                     (mod 2))]
     (apply str (concat (take seconds (cycle [\O])) (repeat (- 1 seconds) \Y)))))
