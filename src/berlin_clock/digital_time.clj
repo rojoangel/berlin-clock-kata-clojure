0e7c650f4958d7268f1ee5d4eb3a95bf8239ed95
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-02-03 20:57:47 +0100

    extract helper 'parse-*-*-row' functions

--- src/berlin_clock/digital_time.clj
+++ src/berlin_clock/digital_time.clj
@@ -7,36 +7,51 @@
 (defn- parse-lamp-row [time & {:keys [start end lamp-on-fn lamp-value]}]
   (apply + (map #(if (lamp-on-fn %) lamp-value 0) (subs time start end))))
 
-(defn seconds [time]
+(defn- parse-seconds-lamp [time]
   (parse-lamp-row time
                   :start 0
                   :end 1
                   :lamp-on-fn (complement on?)
                   :lamp-value 1))
 
+(defn- parse-five-minutes-row [time]
+  (parse-lamp-row time
+                  :start 9
+                  :end 20
+                  :lamp-on-fn on?
+                  :lamp-value 5))
+
+(defn- parse-single-minutes-row [time]
+  (parse-lamp-row time
+                  :start 20
+                  :end 24
+                  :lamp-on-fn on?
+                  :lamp-value 1))
+
+(defn- parse-five-hours-row [time]
+  (parse-lamp-row time
+                  :start 2
+                  :end 6
+                  :lamp-on-fn on?
+                  :lamp-value 5))
+
+(defn- parse-single-hours-row [time]
+  (parse-lamp-row time
+                  :start 6
+                  :end 10
+                  :lamp-on-fn on?
+                  :lamp-value 1))
+
+(defn seconds [time]
+  (parse-seconds-lamp time))
+
 (defn minutes [time]
-  (+ (parse-lamp-row time
-                     :start 9
-                     :end 20
-                     :lamp-on-fn on?
-                     :lamp-value 5)
-     (parse-lamp-row time
-                     :start 20
-                     :end 24
-                     :lamp-on-fn on?
-                     :lamp-value 1)))
+  (+ (parse-five-minutes-row time)
+     (parse-single-minutes-row time)))
 
 (defn hours [time]
-  (+ (parse-lamp-row time
-                     :start 2
-                     :end 6
-                     :lamp-on-fn on?
-                     :lamp-value 5)
-     (parse-lamp-row time
-                     :start 6
-                     :end 10
-                     :lamp-on-fn on?
-                     :lamp-value 1)))
+  (+ (parse-five-hours-row time)
+     (parse-single-hours-row time)))
 
 (defn convert [time]
   (->> time
