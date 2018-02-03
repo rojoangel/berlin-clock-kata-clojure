abe0eecbc76d0a85cecbe92603ed888a8b8c6f9a
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-02-03 20:35:51 +0100

    extract 'on?' helper function

--- src/berlin_clock/digital_time.clj
+++ src/berlin_clock/digital_time.clj
@@ -1,17 +1,20 @@
 (ns berlin-clock.digital-time
   (:require [clojure.string :as str]))
 
+(defn- on? [lamp]
+  (not= \O lamp))
+
 (defn seconds [time]
-  (+ (apply + (map #(if (= \Y %) 0 5) (subs time 0 0)))
-     (apply + (map #(if (= \Y %) 0 1) (subs time 0 1)))))
+  (+ (apply + (map #(if (not (on? %)) 5 0) (subs time 0 0)))
+     (apply + (map #(if (not (on? %)) 1 0) (subs time 0 1)))))
 
 (defn minutes [time]
-  (+ (apply + (map #(if (= \O %) 0 5) (subs time 9 20)))
-     (apply + (map #(if (= \O %) 0 1) (subs time 20 24)))))
+  (+ (apply + (map #(if (on? %) 5 0) (subs time 9 20)))
+     (apply + (map #(if (on? %) 1 0) (subs time 20 24)))))
 
 (defn hours [time]
-  (+ (apply + (map #(if (= \O %) 0 5) (subs time 2 6)))
-     (apply + (map #(if (= \O %) 0 1) (subs time 6 10)))))
+  (+ (apply + (map #(if (on? %) 5 0) (subs time 2 6)))
+     (apply + (map #(if (on? %) 1 0) (subs time 6 10)))))
 
 (defn convert [time]
   (str/join ":" (map #(format "%02d" %) ((juxt hours minutes seconds) time))))
